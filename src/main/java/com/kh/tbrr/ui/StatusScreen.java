a2d6b15d0e0d81ec2f5d1fe07db9578415885efb
KH <kanahazi1st@gmail.com>
2026-08-15 19:49:34 +0900

    微修正

--- src/main/java/com/kh/tbrr/ui/StatusScreen.java
+++ src/main/java/com/kh/tbrr/ui/StatusScreen.java
@@ -15,8 +15,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
